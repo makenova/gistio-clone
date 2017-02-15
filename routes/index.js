6b993a4308f2e11afdc2070db291f5d8e008174a
makenova <themarchmyth@gmail.com>
2017-02-15 00:03:02 -0600

    refactor: shift sand around

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
-// Here there be Dragons that spew messy code.
-// Caveat Lector
-
 var gist = require('../services/gist');
 
 module.exports = function(app){
-  app.get('/', function(req, res){
+  app.get('/', function(req, res, next){
     res.render('index', { title:'gistio-clone'} );
   });
 
@@ -12,14 +9,18 @@ module.exports = function(app){
     var gistid = req.params.gistid;
 
     gist.getgist( gistid , function( err, gistData ){
-      if (err) { return next(err); }
+      if (err) return next(err);
+
       return res.render( 'gist', {
-        title: gistData.title, author: gistData.user,
-        url: gistData.url, files: gistData.files} );
+        title: gistData.title,
+        author: gistData.user,
+        files: gistData.files,
+        url: gistData.url
+      });
     });
   });
 
-  app.get('/*', function(req, res, next){
+  app.get('*', function(req, res, next){
     return next(new Error('That is not an available page'));
   });
 };
--- services/gist.js
+++ services/gist.js
@@ -1,5 +1,5 @@
-var request = require('request'),
-    marked = require('marked');
+var request = require('request');
+var marked = require('marked');
 
 marked.setOptions({
   renderer: new marked.Renderer(),
@@ -8,49 +8,50 @@ marked.setOptions({
   breaks: false
 });
 
-// make the request to github for the gist id provided
 // TODO: serve gists from cache if no changes have been made since last request.
-exports.getgist = function ( gistid, cb ){
+exports.getgist = function ( gistid, callback ){
   var options = {
     url:'https://api.github.com/gists/' + gistid,
     headers: {'User-Agent': 'gistio-clone'}
   };
-  var callback = function (err, res, body){
-    if (!err && res.statusCode == 200){
-      var data = JSON.parse(res.body);
-      var user = data.owner.login || 'unknown';
 
-      prepFiles( data.files, function(err, files){
-        return cb(null, {
-          'title': data.description,
-          'user': user,
-          'url': data.html_url,
-          'files': files
-        });
-      });
-    } else{
-      return cb(err,{});
+  request(options, (err, res, body) => {
+    if (err) return callback(err);
+    if (res.statusCode !== 200) {
+      return callback(new Error('Status Code: ' + res.statusCode));
     }
-  };
 
-  request(options, callback);
+    var data = JSON.parse(res.body);
+    var user = data.owner.login || 'unknown';
+
+    prepFiles(data.files, function(err, files){
+      return callback(null, {
+        'title': data.description,
+        'user': user,
+        'url': data.html_url,
+        'files': files
+      });
+    });
+  });
 };
 
-// loop over the files that are returned form github
-// if file is markdown, convert it to html with marked
-// if code, append and prepend pre and code tags to it
 // TODO Order the list of files in asciibetical order
 function prepFiles (files, callback) {
   var fileArray = [];
-  for (var file in files) {
-    fileArray.push(files[file]);
-    if (files[file].language === "Markdown"){
-      fileArray[fileArray.length-1].content = marked(fileArray[fileArray.length-1].content);
-    } else if (files[file].language){
-      files[file].content = '<pre><code class="language-' +
-      (files[file].language).toLowerCase() + '">' +
-      files[file].content +'</pre></code>';
+
+  for (let fileKey in files) {
+    let file = files[fileKey];
+
+    if (file.language === 'Markdown') {
+      file.content = marked(file.content)
+      fileArray.push(file);
+    } else if (file.language) {
+      file.content = `<pre><code class="language-${file.language.toLowerCase()}">${file.content}</pre></code>`;
+      fileArray.push(file);
+    } else {
+      fileArray.push(file);
     }
   }
+
   return callback(null, fileArray);
 }
